c4102298d8c2ef762e267731d60c146e3839f6c6
Jack Williams <jack.w141@outlook.com>
2019-08-05 00:29:06 +0100

    Added styling to Scaffold & AppBar etc

--- lib/buildConfigurator.dart
+++ lib/buildConfigurator.dart
@@ -9,6 +9,11 @@ class _BuildConfigurationState extends State<BuildConfiguration> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff21213b),
+      appBar: AppBar(
+        backgroundColor: Color(0xff21213b),
+        title: Text('Build Configuration'),
+      ),
       body: Center(
         child: Text('Testing'),
       ),
