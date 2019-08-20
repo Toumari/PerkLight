9cfabcfa50f2c5ce772256bdce2125f0ef080a01
Jack Williams <jack.w141@outlook.com>
2019-08-20 22:45:37 +0100

    Edited Perk model to include a description property for the perks.

--- lib/widgets/perk.dart
+++ lib/widgets/perk.dart
@@ -1,14 +1,16 @@
 class Perk {
   String perkName;
+  String perkDescription;
   String iconName;
   bool isEnabled;
   int id;
 
-  Perk(this.perkName, this.isEnabled,this.id, this.iconName);
+  Perk(this.perkName, this.isEnabled,this.id, this.iconName, this.perkDescription);
 
   Perk.fromJson(Map<String, dynamic> m) {
     perkName = m['perkName'];
     isEnabled = m['isEnabled'];
+    perkDescription = m['perkDescription'];
     id = m['id'];
     iconName = m['iconName'];
   }
@@ -17,15 +19,16 @@ class Perk {
   bool get _isEnabled => isEnabled;
   int get _id => id;
   String get _iconName => iconName;
+  String get _perkDescription => perkDescription;
 
   Map<String, dynamic> toJson() => {
     'perkName': _perkName,
     'isEnabled': _isEnabled,
     'id': _id,
     'iconName': _iconName,
+    'perkDescription': _perkDescription,
   };
 }
-
 
 List fullList = ['Ace in the Hole', 'Adrenaline', 'Aftercare', 'Alert', 'Autodidact', 'Balanced Landing', 'Boil Over', 'Bond', 'Borrowed Time', 'Botany Knowledge', 'Breakdown', 'Buckle Up', 'Calm Spirit', 'Dance With Me', 'Dark Sense', 'Dead Hard', 'Decisive Strike', 'Déja Vu', 'Deliverance', "Detective's Hunch", 'Distortion', 'Diversion', 'Empathy', 'Flip-Flop', 'Head On', 'Hope', 'Iron Will', 'Kindred', 'Leader', 'Left Behind', 'Lightweight', 'Lithe', 'Mettle of Man', 'No Mither', 'No One Left Behind', 'Object of Obsession', 'Open-Handed', 'Pharmacy', "Plunderer's Instinct", 'Poised', 'Premonition', 'Prove Thyself', 'Quick & Quiet', 'Resilience', 'Saboteur', 'Self-Care', 'Slippery Meat', 'Small Game', 'Sole Survivor', 'Solidarity', 'Spine Chill', 'Sprint Burst', 'Stake Out', 'Streetwise', 'This Is Not Happening', 'Technician', 'Tenacity', 'Up the Ante', 'Unbreakable', 'Urban Evasion', 'Vigil', 'Wake Up!', "We'll Make It", "We're Gonna Live Forever", 'Windows of Opportunity',
   "A Nurse's Calling", 'Agitation', 'Bamboozle', 'Barbecue & Chilli', 'Beast of Prey', 'Bitter Murmur', 'Bloodhound', 'Blood Warden', 'Brutal Strength', 'Corrupt Intervention', 'Coulrophobia', 'Dark Devotion', 'Deerstalker', 'Discordance', 'Distressing', 'Dying Light', 'Enduring', 'Fire Up', "Franklin's Demise", 'Furtive Chase', "Hangman's Trick", 'Hex: Devour Hope', 'Hex: Haunted Grounds', 'Hex: Huntress Lullaby', 'Hex: No One Escapes Death', 'Hex: Ruin', 'Hex: The Third Seal', 'Hex: Thrill of the Hunt', "I'm All Ears", 'Infectious Fright', 'Insidious', 'Iron Grasp', 'Iron Maiden', 'Knock Out', 'Lightborn', 'Mad Grit', 'Make Your Choice', 'Monitor & Abuse', 'Monstrous Shrine', 'Overcharge', 'Overwhelming Presence', 'Play With Your Food', 'Pop Goes the Weasel', 'Predator', 'Rancor', 'Remember Me', 'Save the Best for Last', 'Shadowborn', 'Sloppy Butcher', 'Spies from the Shadows', 'Spirit Fury', 'Stridor', 'Surveillance', 'Territorial Imperative', 'Tinkerer', 'Thanataphobia', 'Thrilling Tremors', 'Unnerving Presence', 'Unrelenting', 'Whispers'
@@ -50,10 +53,11 @@ List survivorList = [
 ];
 
 
+
 List<Perk> returnAll() {
   List<Perk> allPerks = List<Perk>();
   for(var i=0;i<fullList.length;i++) {
-    allPerks.add(Perk(fullList[i], true,i,"${i + 1}.png"));
+    allPerks.add(Perk(fullList[i], true,i,"${i + 1}.png",''));
   }
   return allPerks;
 }
@@ -61,7 +65,7 @@ List<Perk> returnAll() {
 List<Perk> returnKiller() {
   List<Perk> killerPerks = List<Perk>();
   for(var i=0;i<killerList.length;i++) {
-    killerPerks.add(Perk(killerList[i],true,i,"${i + 1}.png"));
+    killerPerks.add(Perk(killerList[i],true,i,"${i + 1}.png",''));
   }
   return killerPerks;
 }
@@ -69,7 +73,7 @@ List<Perk> returnKiller() {
 List<Perk> returnSurvivor() {
   List<Perk> survivorPerks = List<Perk>();
   for(var i=0;i<survivorList.length;i++) {
-    survivorPerks.add(Perk(survivorList[i],true,i,"${i + 1}.png"));
+    survivorPerks.add(Perk(survivorList[i],true,i,"${i + 1}.png",''));
   }
   return survivorPerks;
 }
