692208dcc1117bb5aba99df08f178ba4cf17183c
Jack Williams <jack.w141@outlook.com>
2019-08-24 16:34:34 +0100

    Added scrollbar ( sorry Dave )

--- lib/pages/perkDetail.dart
+++ lib/pages/perkDetail.dart
@@ -19,17 +19,19 @@ class _PerkDetailState extends State<PerkDetail> {
         backgroundColor: Color(0xff21213b),
         title: Text("Perk - ${widget.perk.name}", style: TextStyle(color: Colors.white)),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Image.asset(widget.perk.iconPath, height: 200, scale: 0.8),
-            Text(widget.perk.name, style: TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
-            SizedBox(height: 15),
-            Text(widget.perk.description, overflow: TextOverflow.visible, style: TextStyle(color: Colors.white, fontSize: 18), textAlign: TextAlign.left)
-          ],
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Image.asset(widget.perk.iconPath, height: 200, scale: 0.8),
+              Text(widget.perk.name, style: TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+              SizedBox(height: 15),
+              Text(widget.perk.description, overflow: TextOverflow.visible, style: TextStyle(color: Colors.white, fontSize: 18), textAlign: TextAlign.left)
+            ],
+          ),
         ),
       ),
     );
