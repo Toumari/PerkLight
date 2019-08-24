4e2ab37e35dcf995541142068b111a325f6cdec3
Jack Williams <jack.w141@outlook.com>
2019-08-24 16:26:20 +0100

    Implemented basic scrolling functionality into the PerkDetail page. This will only become available if the page's height exceeds the device view size

--- lib/pages/perkDetail.dart
+++ lib/pages/perkDetail.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/perk.dart';
 
-
 class PerkDetail extends StatefulWidget {
   PerkDetail(this.perk);
 
@@ -22,15 +21,17 @@ class _PerkDetailState extends State<PerkDetail> {
       ),
       body: Container(
         padding: EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Image.asset(widget.perk.iconPath, height: 200, scale: 0.8),
-            Text(widget.perk.name, style: TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
-            SizedBox(height: 15),
-            Text(widget.perk.description, style: TextStyle(color: Colors.white, fontSize: 18), textAlign: TextAlign.left)
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Image.asset(widget.perk.iconPath, height: 200, scale: 0.8),
+              Text(widget.perk.name, style: TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+              SizedBox(height: 15),
+              Text(widget.perk.description, overflow: TextOverflow.visible, style: TextStyle(color: Colors.white, fontSize: 18), textAlign: TextAlign.left)
+            ],
+          ),
         ),
       ),
     );
