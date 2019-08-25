c8d3029b182695302ce7f5fb38bb60a8ddafaa66
David Goodson <me@daveg.me.uk>
2019-08-25 07:26:03 +0100

    Add decoder

--- lib/perkSerialiser.dart
+++ lib/perkSerialiser.dart
@@ -20,7 +20,37 @@ class PerksSerialiser {
     return base64UrlEncode(uInt8Bytes);
   }
 
-  static List<int> decode(String perks) {
-    return <int>[];
+  static Map<String, dynamic> decode(String perks) {
+    // Decode Bas64 string
+    Uint8List uInt8Bytes = base64Decode(perks);
+    assert(uInt8Bytes.length == 6);
+
+    // Split
+    Uint8List uInt8PerkIds = uInt8Bytes.sublist(0, 4);
+    Uint8List uInt8Chksum = uInt8Bytes.sublist(4);
+
+    // CRC16 Checksum assertion
+    Uint16List uInt16Chksum = uInt8Chksum.buffer.asUint16List();
+    int chksum = Crc16Usb().convert(uInt8PerkIds);
+    assert(chksum == uInt16Chksum[0]);
+
+    // Calculate perkType from 8th bit and ensure all bytes are the same
+    int sum = uInt8PerkIds.reduce((total, next) => total + (next & 128)) ~/ 128;
+    assert(sum == 0 || sum == 4);
+
+    bool perkType;
+    switch(sum) {
+      case 0:
+        perkType = false;
+        break;
+      case 4:
+        perkType = true;
+    }
+
+    int _perkType = perkType ? 1 : 0;
+    int bitwise = 128 * _perkType;
+
+    List<int> perkIds = uInt8PerkIds.map((item) => item ^ bitwise).toList();
+    return {'list': perkIds, 'type': perkType};
   }
 }
