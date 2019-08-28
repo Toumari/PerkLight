7a85cb1f48af190b605ab5353c80d61e0363380c
Jack Williams <jack.w141@outlook.com>
2019-08-28 21:38:49 +0100

    Specced out a basic version of the character lists that will be clickable ( opening the full character bio )

--- lib/widgets/characterTile.dart
+++ lib/widgets/characterTile.dart
@@ -11,13 +11,25 @@ class CharacterTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Container(
-      child: ListTile(
-        contentPadding: EdgeInsets.all(8),
-        leading: Image.asset(characterImage, fit: BoxFit.fill,),
-        title: Text(name, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 26),),
-        subtitle: Text(description, style: TextStyle(color: Colors.white, fontSize: 14),)
-      ),
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Container(
+            color: Colors.white24,
+              child: Image.asset(characterImage, height: 100,)),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 4.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(name, textAlign: TextAlign.left, style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+              Text(description, textAlign: TextAlign.left, overflow: TextOverflow.clip, style: TextStyle(color: Colors.white70, fontSize: 14, fontWeight: FontWeight.w400),)
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
