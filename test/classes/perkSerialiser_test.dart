9fa647c234f2bf2a1753b712947f94c92c2a4f5c
Matthew Driver <mattdriver001@gmail.com>
2022-07-18 19:46:26 +0100

    fixed issues with crc library since updating packages

--- test/classes/perkSerialiser_test.dart
+++ test/classes/perkSerialiser_test.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 
 import 'package:test/test.dart';
 
-import 'package:crclib/crclib.dart';
+import 'package:crclib/catalog.dart';
 
 import 'package:perklight/classes/perkSerialiser.dart';
 import 'package:perklight/classes/perk.dart';
@@ -60,9 +60,10 @@ void main() {
 
   group('valid input', () {
     group('encode', () {
-      for(PerkType type in PerkType.values) {
-        for(List<int> value in validLists) {
-          String output = PerksSerialiser.encode(perksIds: value, perkType: type);
+      for (PerkType type in PerkType.values) {
+        for (List<int> value in validLists) {
+          String output =
+              PerksSerialiser.encode(perksIds: value, perkType: type);
           Uint8List buffer = base64Decode(output);
 
           group('$value => $output as $type', () {
@@ -78,24 +79,29 @@ void main() {
               Uint8List decodedChksum = buffer.sublist(4);
 
               Uint16List actual = decodedChksum.buffer.asUint16List();
-              Uint16List chksum = Uint16List.fromList([Crc16Usb().convert(decodedList)]);
+              Uint16List chksum = Uint16List.fromList(
+                  [Crc16Usb().convert(decodedList).hashCode]);
 
               expect(actual, equals(chksum));
             });
 
             test('type bit count is valid', () {
               Uint8List decodedList = buffer.sublist(0, 4);
-              int actual = decodedList.reduce((total, next) => total + (next & 128)) ~/ 128;
+              int actual =
+                  decodedList.reduce((total, next) => total + (next & 128)) ~/
+                      128;
 
               expect(actual, anyOf(equals(0), equals(4)));
             });
 
             test('type is valid', () {
               Uint8List decodedList = buffer.sublist(0, 4);
-              int sum = decodedList.reduce((total, next) => total + (next & 128)) ~/ 128;
+              int sum =
+                  decodedList.reduce((total, next) => total + (next & 128)) ~/
+                      128;
 
               PerkType actual;
-              switch(sum) {
+              switch (sum) {
                 case 0:
                   actual = PerkType.survivor;
                   break;
@@ -110,7 +116,6 @@ void main() {
             test('length is valid', () {
               expect(buffer, hasLength(4 + 2));
             });
-
           });
         }
       }
@@ -118,9 +123,10 @@ void main() {
 
     // Decoding tests
     group('decode', () {
-      for(PerkType type in PerkType.values) {
-        for(List<int> value in validLists) {
-          String build = PerksSerialiser.encode(perksIds: value, perkType: type);
+      for (PerkType type in PerkType.values) {
+        for (List<int> value in validLists) {
+          String build =
+              PerksSerialiser.encode(perksIds: value, perkType: type);
 
           group('$build => $value as $type', () {
             var output = PerksSerialiser.decode(build);
@@ -132,38 +138,40 @@ void main() {
             test('type is valid', () {
               expect(output['type'], equals(type));
             });
-
           });
         }
       }
-
     });
-
   });
 
   group('invalid range input', () {
     group('encode', () {
-      for(List<int> value in invalidRangeLists) {
+      for (List<int> value in invalidRangeLists) {
         test('$value raises range exception', () {
-          expect(() => PerksSerialiser.encode(perksIds: value, perkType: PerkType.survivor), throwsRangeError);
+          expect(
+              () => PerksSerialiser.encode(
+                  perksIds: value, perkType: PerkType.survivor),
+              throwsRangeError);
         });
       }
     });
-
   });
 
   group('invalid size input', () {
     group('encode', () {
-      for(List<int> value in invalidListSizes) {
+      for (List<int> value in invalidListSizes) {
         test('$value raises range exception', () {
-          expect(() => PerksSerialiser.encode(perksIds: value, perkType: PerkType.survivor), throwsRangeError);
+          expect(
+              () => PerksSerialiser.encode(
+                  perksIds: value, perkType: PerkType.survivor),
+              throwsRangeError);
         });
       }
     });
   });
 
   group('valid decode', () {
-    for(String value in validStrings) {
+    for (String value in validStrings) {
       test('$value valid decode', () {
         expect(() => PerksSerialiser.decode(value), returnsNormally);
       });
@@ -171,7 +179,7 @@ void main() {
   });
 
   group('invalid input length', () {
-    for(String value in invalidLengthStrings) {
+    for (String value in invalidLengthStrings) {
       test('$value invalid length', () {
         expect(() => PerksSerialiser.decode(value), throwsRangeError);
       });
@@ -179,17 +187,23 @@ void main() {
   });
 
   group('invalid input checksum', () {
-    for(String value in invalidChecksum) {
+    for (String value in invalidChecksum) {
       test('$value invalid checksum', () {
-        expect(() => PerksSerialiser.decode(value), throwsA(allOf(isException, predicate((e) => e.message == 'Checksum invalid'))));
+        expect(
+            () => PerksSerialiser.decode(value),
+            throwsA(allOf(isException,
+                predicate((e) => e.message == 'Checksum invalid'))));
       });
     }
   });
 
   group('invalid input type', () {
-    for(String value in invalidType) {
+    for (String value in invalidType) {
       test('$value invalid type', () {
-        expect(() => PerksSerialiser.decode(value), throwsA(allOf(isException, predicate((e) => e.message == 'Invalid type decode'))));
+        expect(
+            () => PerksSerialiser.decode(value),
+            throwsA(allOf(isException,
+                predicate((e) => e.message == 'Invalid type decode'))));
       });
     }
   });
